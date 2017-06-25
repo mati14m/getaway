8d0e676d70a1e9c54e1e134d0c7600018cba0347
Mefju <mati13m@gmail.com>
2017-06-25 15:57:22 +0200

    new

--- Assets/_Scripts/DoorControler.cs
+++ Assets/_Scripts/DoorControler.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class DoorControler : MonoBehaviour
 {
-
-
+    public int type; //1-N 2-E 3-S 4-W
     public GameObject nextDoor;
     // Use this for initialization
     void Start()
@@ -21,7 +20,34 @@ public class DoorControler : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
-            other.gameObject.transform.position = nextDoor.transform.position-new Vector3(0,-1,-1);
+        if (other.tag == "Player")
+        {
+
+            other.gameObject.transform.position = nextDoor.GetComponent<DoorControler>().getPlayerPosition();
+        }
+            
+
+    }
+
+    private Vector3 getPlayerPosition()
+    {
+        Vector3 vec;
+        if (type == 1)
+        {
+            vec = transform.position - new Vector3(0, -1, 1);
+        }
+        else if (type == 2)
+        {
+            vec = transform.position - new Vector3(-1, -1, 0);
+        }
+        else if(type == 3)
+        {
+            vec = transform.position - new Vector3(0, -1, -1);
+        }
+        else
+        {
+            vec = transform.position - new Vector3(1, -1, 0);
+        }
+        return vec;
     }
 }
--- Assets/_Scripts/PlayerControler.cs
+++ Assets/_Scripts/PlayerControler.cs
@@ -27,12 +27,13 @@ public class PlayerControler : MonoBehaviour {
 
         transform.position = new Vector3(pos.x , pos.y, pos.z);
         Camera mycam = GetComponent<Camera>();
-        float sensitivity = 0.05f;
+        float sensitivity = 0.8f;
 
         Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x-x+mycam.pixelWidth/2, Input.mousePosition.y-y+ mycam.pixelHeight / 2, mycam.nearClipPlane));
+        //Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
         x = Input.mousePosition.x;
         y = Input.mousePosition.y;
-        Debug.Log(x + "  " + y);
+        //Debug.Log(x + "  " + y);
         vp.x -= 0.5f;
         vp.y -= 0.5f;
         vp.x *= sensitivity;
